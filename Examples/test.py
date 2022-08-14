fec27f6a7270fd7ef3cacf47c9aaa589e928d88a
thom <hxy449999@gmail.com>
2022-08-14 15:06:57 +0800

    意识到改变了 project 的分辨率，之前建立的时间线只要勾选了 use project setting 就会跟着改变。暂时无解了。

--- Examples/test.py
+++ Examples/test.py
@@ -50,46 +50,15 @@ for sub_folder in root_folder.GetSubFolderList():
         clip_height: int = int(clip.GetClipProperty()["Resolution"].split("x")[1])
         if clip_height == 1080:
             timeline_name = f"{sub_folder_name}_{str(clip_width)}x{str(clip_height)}"
-            project.SetSetting("timelineResolutionWidth", str(clip_width))
-            project.SetSetting("timelineResolutionHeight", str(clip_height))
+            project.SetSetting("timelineResolutionWidth", str(int(clip_width)))
+            project.SetSetting("timelineResolutionHeight", str(int(clip_height)))
             media_pool.SetCurrentFolder("_Timeline")
             media_pool.CreateEmptyTimeline(timeline_name)
-            for clip in sub_folder.GetClipList():
-                media_pool.AppendToTimeline(clip)
+            media_pool.AppendToTimeline(clip)
         elif clip_width > 1080:
             timeline_name = f"{sub_folder_name}_{str(int(clip_width / 2))}x{str(int(clip_height / 2))}"
-            project.SetSetting("timelineResolutionWidth", str(clip_width / 2))
-            project.SetSetting("timelineResolutionHeight", str(clip_height / 2))
+            project.SetSetting("timelineResolutionWidth", str(int(clip_width / 2)))
+            project.SetSetting("timelineResolutionHeight", str(int(clip_height / 2)))
             media_pool.SetCurrentFolder("_Timeline")
             media_pool.CreateEmptyTimeline(timeline_name)
-            for clip in sub_folder.GetClipList():
-                media_pool.AppendToTimeline(clip)
-
-    # # 得到 root folder 下各机位素材帧率的 list（使用 dict.fromkey 移除 list 中的重复项）
-    # FPS_list_camera = []  # root folder 下各机位素材帧率的空 list
-    # for clip in sub_folder.GetClipList():
-    #     FPS_list_camera.append(clip.GetClipProperty()["FPS"])
-    # FPS_list_camera = list(dict.fromkeys(FPS_list_camera))
-
-    # sub_folder_name = sub_folder.GetName()  # 先拿到 root folder 下子文件夹的名称作为后面新建时间线的名称
-    # if len(FPS_list_camera) == 1:  # 如果这个 list 里面只有一个元素，那么直接设置 fps 为 25，然后以此新建时间线。
-    #     project.SetSetting("timelineResolutionWidth", "1920")
-    #     project.SetSetting("timelineResolutionHeight", "1080")
-    #     project.SetSetting("timelineFrameRate", str(FPS_list_camera[0]).split(".")[0])
-    #     media_pool.CreateEmptyTimeline(sub_folder_name)
-    #     for clip in sub_folder.GetClipList():
-    #         media_pool.AppendToTimeline(clip)
-    # else:
-    #     for i in FPS_list_camera:
-    #         project.SetSetting("timelineResolutionWidth", "1920")
-    #         project.SetSetting("timelineResolutionHeight", "1080")
-    #         project.SetSetting("timelineFrameRate", str(i).split(".")[0])
-    #         media_pool.CreateEmptyTimeline(f"{sub_folder_name}_{str(i).split('.')[0]}")
-    #         for clip in sub_folder.GetClipList():
-    #             media_pool.AppendToTimeline(clip)
-
-# for index, fps in enumerate(FPS_list):
-#     project.SetSetting("timelineFrameRate", fps)
-#     project.SetSetting("timelineResolutionWidth", "1920")
-#     project.SetSetting("timelineResolutionHeight", "1080")
-#     media_pool.CreateTimelineFromClips(sub_folder_name[index], [all_clips[0][0], all_clips[0][1]])
+            media_pool.AppendToTimeline(clip)
