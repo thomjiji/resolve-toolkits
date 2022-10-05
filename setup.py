c15a518f46a6aa92ce108dd073e11a507f2f3ac8
thomjiji <hxy449999@gmail.com>
2022-10-05 13:04:22 +0800

    minor tweaks in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-with open("requirements.txt", "r") as f:
+with open("requirements.txt", "r", encoding="utf-8") as f:
     install_requires = f.read().splitlines()
 
 setuptools.setup(
     name="resolve-toolkits",
-    packages=["resolve-toolkits"],
+    packages=["resolve_toolkits"],
     install_requires=install_requires,
 )
--- src/resolve_toolkits/resolve.py
+++ src/resolve_toolkits/resolve.py
@@ -17,7 +17,9 @@ class Resolve:
         self.timeline = self.project.GetCurrentTimeline()
 
     def get_all_timeline(self) -> List[Timeline]:
-        """Get all existing timelines. Return a list containing all the timeline object."""
+        """Get all existing timelines. Return a list containing all the timeline
+        object.
+        """
         all_timeline = []
         for timeline_index in range(1, self.project.GetTimelineCount() + 1, 1):
             all_timeline.append(self.project.GetTimelineByIndex(timeline_index))
@@ -26,11 +28,13 @@ class Resolve:
     def get_timeline_by_name(self, timeline_name: str) -> Timeline:
         """Get timeline object by name."""
         all_timeline = self.get_all_timeline()
-        timeline_dict = {timeline.GetName(): timeline for timeline in all_timeline}
-        return timeline_dict.get(timeline_name, "")
+        timeline_dict = {timeline.GetName(): timeline for timeline in all_timeline}  # type: ignore
+        return timeline_dict.get(timeline_name, "")  # type: ignore
 
     def get_subfolder_by_name(self, subfolder_name: str) -> Folder:
-        """Get subfolder (Folder object) under the root folder in the media pool."""
+        """Get subfolder (Folder object) under the root folder in the media
+        pool.
+        """
         all_subfolder = self.root_folder.GetSubFolderList()
         subfolder_dict: Dict[str, Folder] = {subfolder.GetName(): subfolder for subfolder in all_subfolder}
-        return subfolder_dict.get(subfolder_name, "")
+        return subfolder_dict.get(subfolder_name, "")  # type: ignore
