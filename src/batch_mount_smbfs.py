0580d7509c288e05ed55873e420789c6d67729a3
thomjiji <hxy449999@gmail.com>
2024-03-29 14:56:47 +0800

    Add argparse for batch_mount_smbfs
    Signed-off-by: thomjiji <hxy449999@gmail.com>

--- src/batch_mount_smbfs.py
+++ src/batch_mount_smbfs.py
@@ -1,15 +1,45 @@
+import argparse
 import subprocess
 from pathlib import Path
 
+MOUNT_POINT = "/Users/thom/Desktop/remote-filesystem"
 
-def replace_special_characters(drive_name):
+
+def replace_special_characters(drive_name: str) -> str:
+    """
+    Replace special characters in the drive name with their percent-encoded values.
+
+    Parameters
+    ----------
+    drive_name
+        The drive name to be encoded.
+
+    Returns
+    -------
+    str
+        The encoded drive name.
+    """
     replacements = {"#": "%23", "@": "%40", ".": "%2E", " ": "%20"}
     for char, encoded_char in replacements.items():
         drive_name = drive_name.replace(char, encoded_char)
     return drive_name
 
 
-def extract_drive_and_server(file_path):
+def extract_drive_and_server(file_path: str) -> dict:
+    """
+    Extract drive names and their corresponding servers (for example gsj-1, gsj-2, ...)
+    from a text file.
+
+    Parameters
+    ----------
+    file_path
+        Path to the text file containing drive and server information.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping server names to their corresponding drive names.
+    """
     drives = {}
     with open(file_path, "r") as file:
         current_server = None
@@ -24,21 +54,51 @@ def extract_drive_and_server(file_path):
     return drives
 
 
-def mount_smbfs(drives, username, password, mount_point):
+def mount_smbfs(drives: dict, username: str, password: str) -> None:
+    """
+    Mount SMB drives based on the provided drive and server information.
+
+    Parameters
+    ----------
+    drives
+        A dictionary mapping server names to their corresponding drive names.
+    username
+        Username for accessing the SMB drives.
+    password
+        Password for accessing the SMB drives.
+    """
     for server, drive_name in drives.items():
-        server_dir = Path(mount_point) / server
+        server_dir = Path(MOUNT_POINT) / server
         (server_dir / drive_name).mkdir(parents=True, exist_ok=True)
         encoded_drive_name = replace_special_characters(drive_name)
         command = f'mount_smbfs -f 0755 -d 0755 //{username}:{password}@{server}/{encoded_drive_name} "{server_dir / drive_name}"'
-        print(command)
         subprocess.run(command, shell=True)
 
 
 if __name__ == "__main__":
-    file_path = "/Users/thom/Desktop/gsj_work/project_files copy.txt"
-    username = "geekshootjack"
-    password = "gsj"
-    mount_point = "/Users/thom/Desktop/remote-filesystem"
+    parser = argparse.ArgumentParser(description="Mount SMB drives from a text file.")
+    parser.add_argument(
+        "-i",
+        "--file_path",
+        type=str,
+        help="Path to the text file containing drive and server information.",
+        required=True,
+    )
+    parser.add_argument(
+        "-u",
+        "--username",
+        type=str,
+        help="Username for accessing the SMB drives.",
+        required=True,
+    )
+    parser.add_argument(
+        "-p",
+        "--password",
+        type=str,
+        help="Password for accessing the SMB drives.",
+        required=True,
+    )
+    args = parser.parse_args()
 
-    drives = extract_drive_and_server(file_path)
-    mount_smbfs(drives, username, password, mount_point)
+    drives = extract_drive_and_server(args.file_path)
+    mount_smbfs(drives, args.username, args.password)
