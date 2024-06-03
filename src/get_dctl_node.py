a412a7d240daeb90135cc510f584ec0056632835
thomjiji <hxy449999@gmail.com>
2024-06-03 18:21:10 +0800

    feat: get_dctl_node.py works initially

--- src/get_dctl_node.py
+++ src/get_dctl_node.py
@@ -1,35 +1,52 @@
-from pprint import pprint
-
+import argparse
+from tabulate import tabulate
 from dri.resolve import Resolve
 
-resolve = Resolve.resolve_init()
-project_manager = resolve.GetProjectManager()
-project = project_manager.GetCurrentProject()
-media_storage = resolve.GetMediaStorage()
-media_pool = project.GetMediaPool()
-root_folder = media_pool.GetRootFolder()
-current_timeline = project.GetCurrentTimeline()
 
-# Iterate through each clip in the video track 1
-for i, timeline_item in enumerate(current_timeline.GetItemListInTrack("video", 1)):
-    node_graph = timeline_item.GetNodeGraph()
-    num_of_nodes = node_graph.GetNumNodes()
+def check_clips_for_dctl():
+    resolve = Resolve.resolve_init()
+    project_manager = resolve.GetProjectManager()
+    project = project_manager.GetCurrentProject()
+    media_storage = resolve.GetMediaStorage()
+    media_pool = project.GetMediaPool()
+    root_folder = media_pool.GetRootFolder()
+    current_timeline = project.GetCurrentTimeline()
 
-    # Check each node for the presence of the "DCTL" tool
-    for node_index in range(1, num_of_nodes + 1):
-        tools_in_node: list[str] = node_graph.GetToolsInNode(node_index)
+    clips_data = []
 
-        # Check if tools_in_node is None
-        if tools_in_node is None:
-            # print(f"Node {node_index} in clip {i+1} has no tools.")
-            continue
+    # Iterate through each clip in the video track 1
+    for i, timeline_item in enumerate(current_timeline.GetItemListInTrack("video", 1)):
+        node_graph = timeline_item.GetNodeGraph()
+        num_of_nodes = node_graph.GetNumNodes()
 
-        # # Use len() to count the number of items in tools_in_node
-        # tool_count = len(tools_in_node)
+        # Check each node for the presence of the "DCTL" tool
+        for node_index in range(1, num_of_nodes + 1):
+            tools_in_node = node_graph.GetToolsInNode(node_index)
 
-        # print(f"Number of tools in node {node_index}: {tool_count}")
+            if tools_in_node is None:
+                continue
 
-        # Check if "OFX: DCTL" tool is in the current node
-        for tool in tools_in_node:
-            if "OFX: DCTL" in tool:
-                pprint(f"Clip {i+1} has DCTL tool used in node {node_index}")
+            # Check if "OFX: DCTL" tool is in the current node
+            for tool in tools_in_node:
+                if "OFX: DCTL" in tool:
+                    clips_data.append([i + 1, timeline_item.GetName(), node_index])
+
+    if clips_data:
+        print(
+            f'The following clips in timeline "{current_timeline.GetName()}" have DCTL tool used:\n\n'
+            f'{tabulate(clips_data, headers=["Clip Number", "Clip Name", "Node Index"], tablefmt="simple")}'
+        )
+    else:
+        print("No clips with DCTL tool found in the specified nodes.")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script to check clips in track 2 of current timeline for DCTL tool usage."
+    )
+    args = parser.parse_args()
+    check_clips_for_dctl()
+
+
+if __name__ == "__main__":
+    main()
