407db7bcbfba8711f391929dcb455af5bb037044
thomjiji <vinyl.gambits.0n@icloud.com>
2025-01-09 16:08:10 +0800

    remove_trailing_char: deprecated

--- src/deprecated/remove_trailing_char_for_stills.py
+++ src/deprecated/remove_trailing_char_for_stills.py
@@ -1,4 +1,12 @@
 """
+Updated notes:
+
+This script is deprecated cause the trailing char "_1" is due to the DaVinci Resolve
+still export mechanism: It will append "_1.3.4" digits in the end of the filename.
+
+So you should remove that "_1.3.4" digits manually from the filename to avoid "_1"
+appending in the still label whiling re-importing them into Resolve.
+
 When importing PowerGrade from another Resolve instance, the label of stills will be
 appended a "_1", this script is used to remove it all for every single powergrade
 imported.
